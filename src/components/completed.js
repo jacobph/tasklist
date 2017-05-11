30d7f92a5f95ba374448d868a932b1ca63adcb31
jacob <jacob.patrick.harris@gmail.com>
2017-05-11 17:24:50 -0400

    hide the completed tasks list unless theres actually something there

--- src/components/completed.js
+++ src/components/completed.js
@@ -12,20 +12,26 @@ class Completed extends Component {
   // }
 
   render() {
-    const tasks = this.props.tasks.map((task, i) => {
-      return(
-        <li key={i} className="list-group-item list-group-item-success"><s>{task}</s></li>
+    if (this.props.tasks.length > 0) {
+      const tasks = this.props.tasks.map((task, i) => {
+        return(
+          <li key={i} className="list-group-item list-group-item-success"><s>{task}</s></li>
+        )
+      });
+      return (
+        <div>
+          <h2>Completed Tasks:</h2>
+          <ul className="list-group">
+            {tasks}
+          </ul>
+          <button className="btn btn-danger" onClick={this.props.handleClick}>Clear Completed Tasks</button>
+        </div>
       )
-    });
-    return (
-      <div>
-        <h2>Completed Tasks:</h2>
-        <ul className="list-group">
-          {tasks}
-        </ul>
-        <button className="btn btn-danger" onClick={this.props.handleClick}>Clear Completed Tasks</button>
-      </div>
-    )
+    } else {
+      return (
+        <div></div>
+      )
+    }
   }
 }
 
